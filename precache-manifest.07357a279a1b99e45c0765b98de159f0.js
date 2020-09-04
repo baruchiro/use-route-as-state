d612a7318e89f4003830aee965d3aec89017b26f
Baruch Odem <baruchiro@gmail.com>
2020-09-04 17:39:59 +0300

    Updates

---
+++ precache-manifest.07357a279a1b99e45c0765b98de159f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f55b7334f3ef2fd1c2b8021253e753cf",
+    "url": "/use-route-as-state/index.html"
+  },
+  {
+    "revision": "aa20d395abb51b08769e",
+    "url": "/use-route-as-state/static/css/main.2c6a48fe.chunk.css"
+  },
+  {
+    "revision": "2f60b217364802c58c37",
+    "url": "/use-route-as-state/static/js/2.557ba7b5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/use-route-as-state/static/js/2.557ba7b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa20d395abb51b08769e",
+    "url": "/use-route-as-state/static/js/main.bc4d1457.chunk.js"
+  },
+  {
+    "revision": "0fe93cfceb7c056dcfdd",
+    "url": "/use-route-as-state/static/js/runtime-main.82207e67.js"
+  }
+]);
