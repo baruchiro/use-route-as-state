dbedc101c6fa733e048cfd103053557bcd69ffd9
Baruch Odem <baruchiro@gmail.com>
2020-09-07 08:11:46 +0300

    Use search list

---
+++ example/src/SearchList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const options = ['Emma', 'Noah', 'Olivia', 'Liam', 'Ava', 'William']
+const subOptions = (search = '') => options.filter((n) =>
+    n.toLowerCase().includes(search.toLowerCase()))
+
+export default ({ search }) => {
+    const s = search || ''
+
+    return <ul>
+        {subOptions(s).map((n) => <li>{n}</li>)}
+    </ul>
+}
--- example/src/UseQueryParam.jsx
+++ example/src/UseQueryParam.jsx
@@ -1,6 +1,7 @@
 import { Box, TextField, Typography } from '@material-ui/core'
 import React from 'react'
 import { useQueryAsState } from 'use-route-as-state'
+import SearchList from './SearchList'
 
 const UseQueryParam = () => {
     const [params, updateParams] = useQueryAsState()
@@ -10,7 +11,7 @@ const UseQueryParam = () => {
     return <Box display='flex' flexDirection='column'>
         <TextField id='qParam' label={`Insert value for 'qParam'`}
             value={params.qParam} onChange={onChange} />
-        <Typography variant='body2'>{params.qParam}</Typography>
+        <Typography variant='body2'><SearchList search={params.qParam} /></Typography>
     </Box>
 }
 
