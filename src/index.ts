a83a62c87ed92982f81bedb293c0b100e8390b21
Baruch Odem <baruchiro@gmail.com>
2020-09-16 12:01:07 +0300

    initialstate and useCallback

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { generatePath, useHistory, useLocation, useRouteMatch } from 'react-router-dom'
 
 const getQueryParamsAsObject = (search: string) => {
@@ -14,28 +14,32 @@ const objectToQueryParams = (obj: Record<string, string>) => '?' + Object.keys(o
     .map((key) => `${key}=${obj[key]}`)
     .join('&')
 
-export const useQueryAsState = (): [Record<string, string>, (updatedParams: Record<string, string>) => void] => {
+export const useQueryAsState = (initialState?: Record<string, string>): [Record<string, string>, (updatedParams: Record<string, string>) => void] => {
     const { pathname, search } = useLocation()
     const history = useHistory()
-    const [params, setParams] = useState({})
+    const [params, setParams] = useState(initialState || {})
 
     useEffect(() => setParams(getQueryParamsAsObject(search)), [search])
 
-    const updateQuery = (updatedParams: Record<string, string>) => {
+    const updateQuery = useCallback((updatedParams: Record<string, string>) => {
         Object.assign(params, updatedParams)
         history.replace(pathname + objectToQueryParams(params))
-    }
+    }, [params, pathname])
 
     return [params, updateQuery]
 }
 
-export const useParamsAsState = (): [Record<string, string>, (updatedParams: Record<string, string>) => void] => {
+export const useParamsAsState = (initialState?: Record<string, string>): [Record<string, string>, (updatedParams: Record<string, string>) => void] => {
     const { path, params } = useRouteMatch()
     const history = useHistory()
+    const [urlParams, setUrlParams] = useState(initialState || {})
 
-    const updateParams = (updatedParams: Record<string, string>) => {
-        Object.assign(params, updatedParams)
-        history.push(generatePath(path, params))
-    }
-    return [params, updateParams]
+    useEffect(() => setUrlParams((uParams) => ({ ...uParams, ...params })), [params])
+
+    const updateParams = useCallback((updatedParams: Record<string, string>) => {
+        Object.assign(urlParams, updatedParams)
+        history.push(generatePath(path, urlParams))
+    }, [path, urlParams])
+
+    return [urlParams, updateParams]
 }
