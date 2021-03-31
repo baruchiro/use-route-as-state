578125431aa650228a37cfe2e1ec95c2491ece6a
Baruch Odem <baruchiro@gmail.com>
2021-03-31 18:21:19 +0300

    Try fix link in example

--- example/src/CreateLinkWithState.jsx
+++ example/src/CreateLinkWithState.jsx
@@ -1,14 +1,14 @@
 import { Box, TextField, Typography } from '@material-ui/core'
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const CreateLinkWithState = () => {
     const [rParam, setRouteParam] = useState()
     const [qParam, setQueryParam] = useState()
 
-    const rp = rParam || ''
-    const qs = qParam !== undefined && qParam !== null ? `?qParam=${qParam}` : ''
-    const to = `/${rp}${qs}`
+    const rp = useMemo(() => encodeURIComponent(rParam || ''), [rParam])
+    const qs = useMemo(() => qParam !== undefined && qParam !== null ? `?qParam=${encodeURIComponent(qParam)}` : '', [qParam])
+    const to = useMemo(() => `/${rp}${qs}`, [rp, qs])
 
     return <Box display='flex' flexDirection='column'>
         <TextField label='Insert value for the Route Param'
